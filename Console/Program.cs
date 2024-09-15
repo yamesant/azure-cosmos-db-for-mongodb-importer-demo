c2d4d39260992c7bb7489c17a3de0c70eba03c74
yamesant <165981998+yamesant@users.noreply.github.com>
2024-09-15 18:12:36 +1000

    Connect to MongoDB

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,10 +1,23 @@
 ﻿using Bogus;
 using Console;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
 
 Randomizer.Seed = new Random(-1);
 ProductFaker faker = new();
 List<Product> products = faker.Generate(5);
-foreach(Product product in products)
-{
-    System.Console.WriteLine(product);
-}
+
+IConfigurationRoot config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddUserSecrets<Program>()
+    .Build();
+string configSectionName = "ProductsCollectionConfig";
+string connectionString = config[$"{configSectionName}:ConnectionString"]!;
+string databaseName = config[$"{configSectionName}:DatabaseName"]!;
+string collectionName = config[$"{configSectionName}:CollectionName"]!;
+MongoClientSettings settings = MongoClientSettings.FromUrl(new MongoUrl(connectionString));
+MongoClient client = new(settings);
+IMongoDatabase database = client.GetDatabase(databaseName);
+IMongoCollection<Product> collection = database.GetCollection<Product>(collectionName);
+
+System.Console.WriteLine(collection.EstimatedDocumentCount());
